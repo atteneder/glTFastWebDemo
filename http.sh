eabb24fbf8b96eea45f4aef070030bd5794d813e
Andreas Atteneder <andreas.atteneder@gmail.com>
2024-03-28 08:37:11 +0100

    feat: http.sh script auto-detects certificates.

--- http.sh
+++ http.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-# with brotli and SSH enabled
-http-server -b -S
+# Brotli compression support
+params="-b"
+
+# https support
+if [ -f ~/.ssh/cert.pem ] && [ -f ~/.ssh/key.pem ]; then
+    echo "HTTPS with global certificates"
+    params="$params -S -C ~/.ssh/cert.pem -K ~/.ssh/key.pem"
+elif [ -f cert.pem ] && [ -f key.pem ]; then
+    echo "HTTPS with local certificates"
+    params="$params -S"
+fi
+
+echo $params
+http-server $params
+
+# This one supports multithreading via COOP and stuff
+# npx statikk --port 8000 --coi
